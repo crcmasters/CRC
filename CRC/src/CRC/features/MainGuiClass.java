9ded2913a3885e0d651b48921cf23cff1e214e17
Łukasz <ukasz@DESKTOP-EFB7BDE.home>
2016-05-02 22:16:31 +0200

    Cos

--- CRC/src/CRC/features/MainGuiClass.java
+++ CRC/src/CRC/features/MainGuiClass.java
@@ -1,5 +1,38 @@
 package CRC.features;
 
-public class MainGuiClass {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class MainGuiClass extends Application {
+
+	private Stage mStage;
+	private BorderPane rootLayout;
+    private AnchorPane targetView;    
+	public Scene mScene;	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		this.mStage = primaryStage;
+	}
+	
+	public Scene getScene() {
+		return this.mScene;
+	}
+	
+	public Stage getStage() {		
+		return this.mStage;
+	}
+	
+	private void initRootLayout() {
+		
+	}
+	
+	private void initMainWindow() {
+		
+	}
 }
