58f06eeed0b50d974945038f9ee1ad4e6bfb1aa7
Łukasz <ukasz@DESKTOP-EFB7BDE.home>
2016-05-02 22:16:31 +0200

    Cos2

--- CRC/src/CRC/main/RuntimeClass.java
+++ CRC/src/CRC/main/RuntimeClass.java
@@ -1,10 +1,14 @@
 package CRC.main;
 
+import CRC.features.MainGuiClass;
+import javafx.application.Application;
+
 public class RuntimeClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Application.launch(MainGuiClass.class);
 	}
 
 }
